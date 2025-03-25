5a416fb470319329bf82f49a5b63bfbce714347c
igor <igor1_souza@hotmail.com>
2025-03-25 17:24:10 -0400

    mudanca na default url layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,10 +3,10 @@ import { Geist } from "next/font/google";
 // import { ThemeProvider } from "next-themes";
 import "./globals.css";
 
-const defaultUrl = process.env.VERCEL_URL
-  ? `https://${process.env.VERCEL_URL}`
-  : "http://localhost:3000";
-
+const defaultUrl =
+  process.env.NEXT_PUBLIC_SITE_URL || // você define isso na Vercel
+  (process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : "http://localhost:3000");
+  
 export const metadata = {
   metadataBase: new URL(defaultUrl),
   title: "Next.js and Supabase Starter Kit",
