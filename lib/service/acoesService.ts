1910b269f085ada0c57a2cebb318d41085d3fd51
Igor souza <igor1_souza@hotmail.com>
2025-11-14 09:43:33 -0400

    removendo erro de prod importacoes incorretas

--- lib/service/acoesService.ts
+++ lib/service/acoesService.ts
@@ -35,91 +35,94 @@ export async function deleteAcaoItemHistoryById(id: number){
   return result;
 }
 
-// export async function updateAcaoAndUploadImageById(
-//   id: number,
-//   formData: FormData
-// ) {
-//   // 1. Separamos os campos de texto dos arquivos
-//   const textUpdates: Record<string, any> = {};
-//   const files: File[] = [];
 
-//   for (const [key, value] of formData.entries()) {
-//     if (value instanceof File) {
-//       files.push(value);
-//     } else {
-//       textUpdates[key] = value;
-//     }
-//   }
 
-//   //    Só atualizamos o banco se houver campos de texto para atualizar.
-//   if (Object.keys(textUpdates).length > 0) {
-//     await updateAcaoById(id, textUpdates);
-//   }
+// REMOVER ?????
+export async function updateAcaoAndUploadImageById(
+  id: number,
+  formData: FormData
+) {
+  // 1. Separamos os campos de texto dos arquivos
+  const textUpdates: Record<string, any> = {};
+  const files: File[] = [];
 
-//   //    Só executamos a lógica de upload se houver arquivos.
-//   if (files.length > 0) {
-//     for (const file of files) {
-//       const path = `${id}/${Date.now()}-${file.name}`;
-//       const imageUrl = await uploadAzure(file, path);
-//       await addAcaoImageById(id, imageUrl, textUpdates.descricao || "");
-//     }
-//   }
+  for (const [key, value] of formData.entries()) {
+    if (value instanceof File) {
+      files.push(value);
+    } else {
+      textUpdates[key] = value;
+    }
+  }
 
-//   return { message: "Ação atualizada com sucesso!" };
-// }
+  //    Só atualizamos o banco se houver campos de texto para atualizar.
+  if (Object.keys(textUpdates).length > 0) {
+    await updateAcaoById(id, textUpdates);
+  }
+
+  //    Só executamos a lógica de upload se houver arquivos.
+  if (files.length > 0) {
+    for (const file of files) {
+      const path = `${id}/${Date.now()}-${file.name}`;
+      const imageUrl = await uploadAzure(file, path);
+      // await addAcaoImageById(id, imageUrl, textUpdates.descricao || "");
+    }
+  }
+
+  return { message: "Ação atualizada com sucesso!" };
+}
 
 
 // REMOVER ESSA FUNÇÃO
-// export async function createAcoesWithTrilha(input: {
-//   trilha: TrilhaInput;
-//   waypoints: (WaypointInput & { fotos?: File[] })[];
-// }) {
-//   const trilhaRecord = await insertTrilhaData({
-//     nome: input.trilha.nome,
-//     dataInicio: input.trilha.dataInicio ?? null,
-//     dataFim: input.trilha.dataFim ?? null,
-//     duracaoMinutos: input.trilha.duracaoMinutos ?? null,
-//     geom: input.trilha.geom,
-//   });
+export async function createAcoesWithTrilha(input: {
+  trilha: TrilhaInput;
+  waypoints: (WaypointInput & { fotos?: File[] })[];
+}) {
+  const trilhaRecord = await insertTrilhaData({
+    nome: input.trilha.nome,
+    dataInicio: input.trilha.dataInicio ?? null,
+    dataFim: input.trilha.dataFim ?? null,
+    duracaoMinutos: input.trilha.duracaoMinutos ?? null,
+    geom: input.trilha.geom,
+  });
 
-//   const trilhaId = trilhaRecord.id;
+  const trilhaId = trilhaRecord.id;
 
-//   for (const wp of input.waypoints) {
-//     const pointWkt = `POINTZ(${wp.longitude} ${wp.latitude} ${wp.elevation ?? 0})`;
+  for (const wp of input.waypoints) {
+    const pointWkt = `POINTZ(${wp.longitude} ${wp.latitude} ${wp.elevation ?? 0})`;
 
-//     await insertWaypointDataInWaypointsTable({
-//       trilhaId,
-//       nome: wp.name ?? null,
-//       ele: wp.elevation ?? null,
-//       recordedat: wp.time ?? null,
-//       geom: pointWkt,
-//     });
+    await insertWaypointDataInWaypointsTable({
+      trilhaId,
+      nome: wp.name ?? null,
+      ele: wp.elevation ?? null,
+      recordedat: wp.time ?? null,
+      geom: pointWkt,
+    });
 
-//     const acaoRecord = await insertAcaoData({
-//       name: wp.name ?? null,
-//       latitude: wp.latitude.toString(),
-//       longitude: wp.longitude.toString(),
-//       elevation: wp.elevation?.toString() ?? '0',
-//       time: wp.time ?? null,
-//       descricao: wp.descricao ?? null,
-//       mes: wp.mes,
-//       atuacao: wp.atuacao,
-//       acao: wp.acao ?? null,
-//       geom: pointWkt,
-//     });
+    const acaoRecord = await insertAcaoData({
+      name: wp.name ?? null,
+      latitude: wp.latitude.toString(),
+      longitude: wp.longitude.toString(),
+      elevation: wp.elevation?.toString() ?? '0',
+      time: wp.time ?? null,
+      descricao: wp.descricao ?? null,
+      mes: wp.mes,
+      atuacao: wp.atuacao,
+      acao: wp.acao ?? null,
+      geom: pointWkt,
+    });
 
-//     const acaoId = acaoRecord.id;
-//     if (wp.fotos) {
-//       for (const file of wp.fotos) {
-//         const path = `${acaoId}/${Date.now()}-${file.name}`;
-//         const imageUrl = await uploadAzure(file, path);
-//         await addAcaoImageById(acaoId, imageUrl, wp.descricao ?? "");
-//       }
-//     }
-//   }
+    const acaoId = acaoRecord.id;
+    if (wp.fotos) {
+      for (const file of wp.fotos) {
+        const path = `${acaoId}/${Date.now()}-${file.name}`;
+        const imageUrl = await uploadAzure(file, path);
+        // await addAcaoImageById(acaoId, imageUrl, wp.descricao ?? "");
+      }
+    }
+  }
 
-//   return { trilhaId };
-// }
+  return { trilhaId };
+}
 
 
 
