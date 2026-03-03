fc8aec29f181ddf8a7881c13c2043f6011549368
Igor souza <igor1_souza@hotmail.com>
2026-03-03 13:52:25 -0400

    tentando enviar arquivo grande

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   transpilePackages: ['shapefile'],
+  serverActions: {
+    bodySizeLimit: '100mb',
+  },
   webpack: (config) => {
     config.resolve.fallback = { fs: false, path: false, stream: false };
     return config;
